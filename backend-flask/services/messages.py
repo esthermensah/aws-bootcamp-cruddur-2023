c1eddce24f300b93f2cfb9fec8e9ca72f57f8d67
Esther Mensah <edmworkmail@gmail.com>
2025-03-13 23:03:25 +0000

    completed

--- backend-flask/services/messages.py
+++ backend-flask/services/messages.py
@@ -11,16 +11,16 @@ class Messages:
     results = [
       {
         'uuid': '4e81c06a-db0f-4281-b4cc-98208537772a' ,
-        'display_name': 'Esther Mensah',
-        'handle':  'emensah',
-        'message': 'I made it to this point in the bootcamp!',
+        'display_name': 'Andrew Brown',
+        'handle':  'andrewbrown',
+        'message': 'Cloud is very fun!',
         'created_at': now.isoformat()
       },
       {
         'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'display_name': 'Esther Mensah',
-        'handle':  'emensah',
-        'message': 'Learning how cloud works is fufilling ',
+        'display_name': 'Andrew Brown',
+        'handle':  'andrewbrown',
+        'message': 'This platform is great!',
         'created_at': now.isoformat()
     }]
     model['data'] = results
